22c85d5a875a74bf68b249b5c067fdd21de373ec
zingw <vietduc.bk1996@gmail.com>
2023-04-05 22:33:21 +0700

    initial

---
+++ src/main/java/jp/mrduc/me/chatweb/ChatwebApplication.java
@@ -0,0 +1,13 @@
+package jp.mrduc.me.chatweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatwebApplication.class, args);
+    }
+
+}
