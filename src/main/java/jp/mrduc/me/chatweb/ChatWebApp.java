90222596fcfdab64a75dcfa2c443cf67c4edae33
zingw <vietduc.bk1996@gmail.com>
2023-04-05 23:07:09 +0700

    fix libs

--- src/main/java/jp/mrduc/me/chatweb/ChatWebApp.java
+++ src/main/java/jp/mrduc/me/chatweb/ChatWebApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatwebApplication {
+public class ChatWebApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatwebApplication.class, args);
+        SpringApplication.run(ChatWebApp.class, args);
     }
 
 }
